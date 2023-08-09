bd31abf246999e1aca6dc8bc85e0b310f45e938e
Ahmed-Shahi <ahmedshahi038@gmail.com>
2023-08-10 04:13:55 +0500

    done

---
+++ Tables/app.js
@@ -0,0 +1,11 @@
+
+var a = +prompt('Enter a Table No. ');
+var b = +prompt('Enter a starting No. ');
+var c = +prompt('Enter a ending No. ');
+var d;
+
+
+for(d=b; d<=c; d++){
+    document.write(  a ,' x ',d,' = ', a*d, '<br>');
+
+}   
